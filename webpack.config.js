a274dccadf475de191a02a6acc672cd970ce88f4
RococoKid <sara.r.kalkstein@gmail.com>
2023-12-21 00:10:09 -0500

    Co-authored-by: Jordan Woods <ejwoods@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,43 @@ module.exports = {
     filename: 'bundle.js'
   },
 
-  plugins: {
-    
-  }
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
 
-}
+  module: {
+    rules: [
+      {
+        test: /.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /s?css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      }
+    ]
+  },
+
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    compress: true,
+    hot: true,
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+    },
+    proxy: {
+      '**': 'http://localhost:3000',
+    }
+  }
+};
