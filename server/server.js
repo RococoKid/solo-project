ffeb9c80c75a99a7076e04ff9b284e1ae350bdd4
RococoKid <sara.r.kalkstein@gmail.com>
2023-12-27 17:28:14 -0500

    beginning to set up sql tables

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 //json parser
-//catchall route for 404
-//global error handler
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 //This is where we add a question
@@ -13,3 +13,20 @@ app.post('/', /*some middleware here*/, (req, res) => res.status(200).json({}))
 
 //This is where we retrieve a randomly selected question
 app.get('/', /*some middleware here*/, (req, res) => res.status(200).json({}))
+
+//catchall route for 404
+app.use('/', (req, res, next) => {
+    res.status(404).send('Sorry can\'t find that!');
+  });
+
+//global error handler
+app.use((err, req, res, next) => {
+    const defaultErr = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 500,
+      message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  });
