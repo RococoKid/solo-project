70fd5de195547485ca782e96022a7388ee7f9de3
RococoKid <sara.r.kalkstein@gmail.com>
2024-01-05 12:41:31 -0500

    start mapping out react

--- client/App.jsx
+++ client/App.jsx
@@ -2,8 +2,42 @@ import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 
 const App = () => {
+//select a session or create a new one
+    //dropdown with existing sessions you can click
+    //field to type in new session name
+        //submit button for new session
+//ask your question
+    //field where you type q
+    //submit button
+//reveal a random question from your session
+    //button to request question
+    //field where question shows up
+    return (
+        <div>
 
+        </div>
+    );
 };
+
+const Button = ({}) => {
+    return (
+        <button onClick={}>
+
+        </button>
+    )
+};
+
+const Field = ({}) => {
+    return (
+        
+    )
+};
+
+const Menu = ({}) => {
+    return (
+
+    )
+}
 
 const root = createRoot(document.querySelector('#root'));
 root.render(<App />,);
