265d705de5f4b32acfe5df9ea02a80fe4bd9b9fe
RococoKid <sara.r.kalkstein@gmail.com>
2024-01-05 00:38:41 -0500

    develop jsx and html files

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 
+const App = () => {
+
+    return ();
+};
 
 const root = createRoot(document.querySelector('#root'));
 root.render(<App />,);
