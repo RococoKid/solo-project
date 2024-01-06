db956a61af136563643f5284e33627782890fca7
RococoKid <sara.r.kalkstein@gmail.com>
2024-01-05 20:45:37 -0500

    add react components

--- client/App.jsx
+++ client/App.jsx
@@ -8,13 +8,60 @@ const App = () => {
         //submit button for new session
 //ask your question
     //field where you type q
+    const captureQuestion = () => {
+
+    };
     //submit button
+    const sendQuestion = () => {
+
+    };
 //reveal a random question from your session
     //button to request question
+    const [revealedQuestion, setQuestion] = useState([]);
+    const getQuestion = () => {
+        useEffect(() => {
+            fetch('http://localhost:3000/selectQuestion')
+            .then((res) => {
+                return res.json()
+            })
+            .then((data) => {
+                console.log(data);
+                setQuestion(data)
+            })
+        }, [])
+    };
     //field where question shows up
+//define my functions, then pass variable into onclick below
+//fetch requests to send http requests to back end
+//don't worry about drop down for now, just hard code everything to go to test session 1
+
     return (
         <div>
+            <Menu>
+            </Menu>
 
+            <Field>
+            </Field>
+
+            <Button>
+                Start New Session!
+            </Button>
+
+            <Field>
+
+            </Field>
+
+            <Button onClick={sendQuestion}>
+                Submit Your Question!
+            </Button>
+            
+            <Button onClick={getQuestion}>
+                Get A Question!
+            </Button>
+
+            <Field>
+
+            </Field>
         </div>
     );
 };
